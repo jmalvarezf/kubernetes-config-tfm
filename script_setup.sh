3ed9a9a042e2c973da897d43992559c5f121425f
Jose Maria Alvarez <josemaria.alvarezfernandez@elcorteingles.es>
2018-09-08 21:42:31 +0200

    Adapt versions and add kibana visualizations

--- script_setup.sh
+++ script_setup.sh
@@ -4,4 +4,3 @@ curl -X DELETE http://192.168.99.100:30912/events
 curl -v -X POST -d @elastic-connector-config.json -H "Content-type: application/json" http://192.168.99.100:32190/connectors
 curl -f -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: cluster" 'http://192.168.99.100:31096/api/saved_objects/index-pattern/events*' -d'{"attributes":{"title":"events*"}}'
 
-
