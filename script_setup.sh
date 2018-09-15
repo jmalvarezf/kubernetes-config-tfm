86967510cf4799b9c1ef9e3bcdbf416bed7fc0a0
Jose Maria Alvarez <josemaria.alvarezfernandez@elcorteingles.es>
2018-09-15 20:49:23 +0200

    Change of configurations

--- script_setup.sh
+++ script_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -X DELETE http://elasticsearch.elejandria.site:9200/events
-curl -v -X POST -d @elastic-connector-config.json -H "Content-type: application/json" http://kafka-connect.elejandria.site:8083/connectors
+#curl -X DELETE http://elasticsearch.elejandria.site:9200/events
+curl -v -X POST -d @elastic-connector-config_gke.json -H "Content-type: application/json" http://kafka-connect.elejandria.site:8083/connectors
 curl -f -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: cluster" 'http://kibana.elejandria.site/api/saved_objects/index-pattern/events*' -d'{"attributes":{"title":"events*"}}'
 
